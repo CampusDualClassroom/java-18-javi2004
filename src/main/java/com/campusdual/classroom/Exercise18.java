7b4ca5efeb997621df71bd76c0badf821c515b22
Javi Pérez <javiperezotero10@gmail.com>
2024-09-27 09:40:58 +0200

    Ejercicio 18 completado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,30 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		int tamaño = Utils.integer("Ingresa el tamaño del array");
+		int[] array = createAndInitializeArray(tamaño);
+		showInlineArray(array);
+
+	}
+	public static int[] createAndInitializeArray(int num){
+		int[] arra = new int[num];
+		for (int i = 0; i <num; i++) {
+			arra[i] = i+1;
+		}
+		return arra;
+	}
+	public static void showInlineArray(int[] array){
+		for (int i = 0; i <array.length; i++) {
+			if ( i != array.length-1){
+				System.out.print("Numero:"+array[i]+" ");
+			}else {
+				System.out.print("Numero:"+array[i]);
+			}
+		}
 
 	}
 }
